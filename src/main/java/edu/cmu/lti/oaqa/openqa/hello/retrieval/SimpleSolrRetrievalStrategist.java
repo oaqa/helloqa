8447227c703a13ae651cdcf030475936628d8788
Elmer Garduno <garduno@gmail.com>
2012-08-16 17:12:04 -0400

    Initial commit

---
+++ src/main/java/edu/cmu/lti/oaqa/openqa/hello/retrieval/SimpleSolrRetrievalStrategist.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright 2012 Carnegie Mellon University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package edu.cmu.lti.oaqa.openqa.hello.retrieval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import edu.cmu.lti.oaqa.core.provider.solr.SolrWrapper;
+import edu.cmu.lti.oaqa.framework.data.Keyterm;
+import edu.cmu.lti.oaqa.framework.data.RetrievalResult;
+import edu.cmu.lti.oaqa.mergeqa.retrieval.AbstractRetrievalStrategist;
+
+/**
+ * 
+ * @author Zi Yang <ziy@cs.cmu.edu>
+ * 
+ */
+public class SimpleSolrRetrievalStrategist extends AbstractRetrievalStrategist {
+
+  private Integer hitListSize;
+
+  protected SolrWrapper wrapper;
+  
+  @Override
+  public void initialize(UimaContext aContext) throws ResourceInitializationException {
+    super.initialize(aContext);
+    try {
+      this.hitListSize = (Integer) aContext.getConfigParameterValue("hit-list-size");
+    } catch (ClassCastException e) { // all cross-opts are strings?
+      this.hitListSize = Integer.parseInt((String) aContext
+              .getConfigParameterValue("hit-list-size"));
+    }
+    String serverUrl = (String) aContext.getConfigParameterValue("server");
+    Integer serverPort = (Integer) aContext.getConfigParameterValue("port");
+    Boolean embedded = (Boolean) aContext.getConfigParameterValue("embedded");
+    String core = (String) aContext.getConfigParameterValue("core");
+    try {
+      this.wrapper = new SolrWrapper(serverUrl, serverPort, embedded, core);
+    } catch (Exception e) {
+      throw new ResourceInitializationException(e);
+    }
+  }
+
+  @Override
+  protected final List<RetrievalResult> retrieveDocuments(String questionText,
+          List<Keyterm> keyterms) {
+    String query = formulateQuery(keyterms);
+    return retrieveDocuments(query);
+  };
+
+  protected String formulateQuery(List<Keyterm> keyterms) {
+    StringBuffer result = new StringBuffer();
+    for (Keyterm keyterm : keyterms) {
+      result.append(keyterm.getText() + " ");
+    }
+    String query = result.toString();
+    System.out.println(" QUERY: " + query);
+    return query;
+  }
+
+  private List<RetrievalResult> retrieveDocuments(String query) {
+    List<RetrievalResult> result = new ArrayList<RetrievalResult>();
+    try {
+      SolrDocumentList docs = wrapper.runQuery(query, hitListSize);
+      for (SolrDocument doc : docs) {
+        RetrievalResult r = new RetrievalResult((String) doc.getFieldValue("id"), (Float) doc.getFieldValue("score"), query);
+        result.add(r);
+        System.out.println(doc.getFieldValue("id"));
+      }
+    } catch (Exception e) {
+      System.err.println("Error retrieving documents from Solr: " + e);
+    }
+    return result;
+  }
+
+  @Override
+  public void collectionProcessComplete() throws AnalysisEngineProcessException {
+    super.collectionProcessComplete();
+    wrapper.close();
+  }
+}
